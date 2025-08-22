1db9d108bbc366e92b491af6c756632094b7fe3b
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-08-22 17:20:58 +0530

    remove duplicates while retaining the order of elements in an array.java

--- remove duplicates while retaining the order of elements in an array.java
+++ remove duplicates while retaining the order of elements in an array.java
@@ -2,17 +2,16 @@ class Main {
     public static void main(String[] args) {
         int ar[]={3,5,6,6,1,3,7};
         LinkedHashSet<Integer> lhs=new LinkedHashSet<>();
-        int dup=0; int p=0;
+        int unq=0; int p=0;
         for (int e : ar) {
             if(!lhs.contains(e)){
                 lhs.add(e);
-                dup++;
+                unq++;
             }
         }
-        int[] res=new int[dup];
+        int[] res=new int[unq];
         for (int e : lhs) {
             res[p++]=e;
         }
-        System.out.println(Arrays.toString(res));
     }
 }
