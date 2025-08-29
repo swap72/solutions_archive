b59c6fcd3dad3e37ae1b2ee614a74c7b8c5b6a51
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-08-29 19:50:13 +0530

    DigiCount_Recursive.java

---
+++ jutge.org/DigiCount_Recursive.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int numberOfDigits(int n) {
+        if (n < 10) return 1;
+        return 1 + numberOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;           
+            System.out.println(numberOfDigits(n));
+        }
+    }
+}
+
+
+
+/*
+
+jutge.org has very strict judging constraints and also hidden test cases
+
+*/
